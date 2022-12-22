c52b92bbccea2239dfe937ef56d55acfa1f955f6
Monayem Islam <monayem.dev@gmail.com>
2022-12-22 13:17:48 +0600

    Area number added to user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'area'=>['required'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -67,6 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'area_number' => $data['area'],
             'password' => Hash::make($data['password']),
         ]);
     }
