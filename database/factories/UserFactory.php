0d3bbb2067cdc22a85220b3f78473f8daa31b4a0
DESKTOP-9ORVBHL\Monayem Islam Tamal <monayem.dev@gmail.com>
2023-02-10 14:55:53 +0600

    User Factory Created. It will help to create fake data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -16,9 +17,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'role'=>0,
+            'area_number'=>11,
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('tamal123456'), // password
             'remember_token' => Str::random(10),
         ];
     }
